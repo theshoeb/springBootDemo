d901c98aba052a2bed82c2a1c4acc10cc0fd6aac
theshoeb <hisho.sk@gmail.com>
2021-02-06 23:58:54 +0530

    first commit

---
+++ autowire_dependencyInjection/src/main/java/com/example/demo/Alien.java
@@ -0,0 +1,39 @@
+package com.example.demo;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
+
+@Component
+@Scope(value="prototype")
+public class Alien {
+	private int aid;
+	private String aname;
+	private String tech;
+	
+	public Alien() {
+		System.out.print("creating alien.....");
+	}
+	public int getAid() {
+		return aid;
+	}
+	public void setAid(int aid) {
+		this.aid = aid;
+	}
+	public String getAname() {
+		return aname;
+	}
+	public void setAname(String aname) {
+		this.aname = aname;
+	}
+	public String getTech() {
+		return tech;
+	}
+	public void setTech(String tech) {
+		this.tech = tech;
+	}
+	public void show() {
+		System.out.print("born as alien");
+	}
+	
+
+}
---
+++ autowire_dependencyInjection/src/main/java/com/example/demo/AutowireDependencyInjectionApplication.java
@@ -0,0 +1,41 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class AutowireDependencyInjectionApplication {
+
+	public static void main(String[] args) {
+		
+		ConfigurableApplicationContext context=SpringApplication.run(AutowireDependencyInjectionApplication.class, args);
+
+//Even if we comment the lines of Object Creation from 15-16 the Object of Class Alien will be Created because byDefaoult @COMPONET follow SINGLETON SCOPE  so it MAKES object ready even before your call		
+//		Alien a=context.getBean(Alien.class); 
+//		a.show();
+
+		
+// Now what will happen if we try to create two object of Alien 
+// You will see that Spring creates only ONE OBJECT .....since ALIEN is Having SingleTon Scope@COMPONENT
+//		Alien a=context.getBean(Alien.class); 
+//		a.show();
+//		Alien a1=context.getBean(Alien.class);
+//		a1.show();
+		
+		
+// how can we create >=TWO Objects ?? for That we have to ADD the PROTOTTPE SCOPE_Annotation to ALien Class i.e. @SCOPE(Value="prototype");
+//Now Try 
+
+		Alien a=context.getBean(Alien.class); 
+		a.show();
+		Alien a1=context.getBean(Alien.class);
+		a1.show();	
+// we saw two objects were created this time
+// now what if We remove all line for Object Creation what do u think will there be any instance like we get in SINGLETON ???
+// TRIED AND GOT ERROR ??
+// NO! WE WONT GET ANY BYDEFAULT INSATANCE , WE HAVE TO CREATE IT AS PER OUR USE BY .getBean();
+		
+	}
+
+}
